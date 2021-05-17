f99ccb4607c70b74cfbe3d36e038642c9862e944
Marco <markforester93@gmail.com>
2021-05-17 15:48:34 +1000

    Application Basic Functionality Achieved

---
+++ server.js
@@ -0,0 +1,58 @@
+// Dependencies
+
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uniqid = require("uniqid");
+
+// Sets up the Express App
+
+const app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// define working arr
+
+let workingArr;
+
+fs.readFile(path.join(__dirname, "/db/db.json"), "utf-8", (err, data) => {
+  if (err) console.error(err);
+  workingArr = JSON.parse(data);
+});
+
+// Routes
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/db/db.json"))
+);
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+// Create New Note - takes in JSON input
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  newNote.id = uniqid();
+  workingArr.push(newNote);
+  console.log(workingArr);
+  fs.writeFile(
+    path.join(__dirname, "/db/db.json"),
+    JSON.stringify(workingArr),
+    (err, data) => {
+      if (err) console.error(err);
+    }
+  );
+  res.send("Note Added Successfully");
+});
+
+// Starts the server to begin listening
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
